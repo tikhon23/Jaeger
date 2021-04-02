f1d53779ad076549947591bdd617dc22c094726c
Tikhon23 <bartemiys23@gmail.com>
2021-04-02 18:11:43 +0300

    Характеристики робота

---
+++ src/Jaeger.java
@@ -0,0 +1,26 @@
+public class Jaeger {
+    String modelName = "Bracer Phoenix";
+    String mark = "Mark-5";
+    String origin = "USA";
+    float height = (float) 70.1;
+    float weight = (float) 2.1;
+    int speed = 3;
+    int strength = 8;
+    int armor = 9;
+
+    void drift(){
+
+    }
+
+    void move(){
+
+    }
+
+    void scanKaiju(){
+
+    }
+
+    void useVortexCannon(){
+        
+    }
+}
